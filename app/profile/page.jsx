e5e1c710c869a91b371f38effb1febce16ef4537
Alejandro Marin <alejandromarin199901@gmail.com>
2023-06-20 14:52:31 -0300

    Updating profile component

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,7 +6,20 @@ import { useRouter } from 'next/navigation';
 
 import Profile from '@components/Profile';
 
-const Profile = () => {
+const MyProfile = () => {
+    const { data: session } = useSession();
+
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        const fetchPosts = async () => {
+          const response = await fetch(`/api/users/${session?.user.id}/posts`);
+          const data = await response.json();
+    
+          setPosts(data);
+        }
+    
+        if(session?.user.id) fetchPosts();
+    }, []);
 
     const handleEdit = () => {
 
@@ -19,11 +32,11 @@ const Profile = () => {
         <Profile
             name="My"
             desc="Welcome to your personalized profile page"
-            data={[]}
+            data={posts}
             handleEdit={handleEdit}
             handleDelete={handleDelete}
         />
   )
 }
 
-export default Profile
+export default MyProfile
